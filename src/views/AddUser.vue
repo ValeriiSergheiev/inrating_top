556ada03dba091628fd6bacb67a969ba85917642
Valerii <val.sergeyev@gmail.com>
2018-09-07 11:22:56 +0300

    FileReader headnler not ready

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="action-block">
-      <h2>Add new user</h2>
+      <h2>Add new user (manual)</h2>
       <b-form @submit.prevent="addUser">
         <b-form-group
           label="Name"
@@ -34,6 +34,28 @@
         <b-button type="submit" variant="success" class="mr-2">Save</b-button>
         <b-button to="/" variant="secondary">Cancel</b-button>
       </b-form>
+  
+      <hr>
+      
+      <h2>Add new user (from JSON)</h2>
+      <b-form @submit.prevent="addUserFromJson">
+        <b-form-group
+          label="Import Your JSON from file"
+          label-for="json"
+        >
+          <b-form-file
+            id="json"
+            v-model="file"
+            accept=".json"
+            :state="Boolean(file)"
+            placeholder="Choose a file..."
+            @change="handleFiles"
+          ></b-form-file>
+        </b-form-group>
+  
+        <b-button type="submit" variant="success" class="mr-2">Save</b-button>
+        <b-button to="/" variant="secondary">Cancel</b-button>
+      </b-form>
     </div>
   </div>
 </template>
@@ -52,10 +74,14 @@
         name: '',
         surname: '',
         phone: '',
-        email: ''
+        email: '',
+        file: null,
+        selectedFile: null,
+        reader: null
       }
     },
-    mounted() {},
+    mounted() {
+    },
     
     methods: {
       addUser() {
@@ -73,6 +99,19 @@
         )
         localStorage.setItem("usersStorage", JSON.stringify(this.users))
         this.$router.push('/')
+      },
+
+      handleFiles() {
+        this.selectedFile = document.getElementById('json').files[0]
+        this.reader = new FileReader()
+        this.reader.onload = function () {
+          console.log(document.getElementById('json').result)
+        }
+        reader.readAsText(this.selectedFile)
+      },
+
+      addUserFromJson() {
+        // console.log(this.selectedFile)
       }
     },
     
